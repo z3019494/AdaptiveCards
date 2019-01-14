94b6a134a6553bab1eb3df6528ef993a6685c8f0
Carlos Zamora <cazamor@microsoft.com>
2019-01-14 15:33:01 -0800

    Android: TileXY works. Need to fix Tile X/Y w/ alignment

--- source/android/adaptivecards/src/main/java/io/adaptivecards/renderer/AdaptiveCardRenderer.java
+++ source/android/adaptivecards/src/main/java/io/adaptivecards/renderer/AdaptiveCardRenderer.java
@@ -3,17 +3,23 @@ package io.adaptivecards.renderer;
 import android.content.Context;
 import android.graphics.Bitmap;
 import android.graphics.Color;
+import android.graphics.Shader;
 import android.graphics.drawable.BitmapDrawable;
+import android.graphics.drawable.InsetDrawable;
 import android.support.v4.app.FragmentManager;
 import android.view.Gravity;
 import android.view.View;
 import android.view.ViewGroup;
+import android.view.ViewParent;
 import android.widget.HorizontalScrollView;
+import android.widget.ImageView;
 import android.widget.LinearLayout;
 
 import io.adaptivecards.objectmodel.ActionAlignment;
 import io.adaptivecards.objectmodel.ActionsOrientation;
 import io.adaptivecards.objectmodel.AdaptiveCard;
+import io.adaptivecards.objectmodel.BackgroundImage;
+import io.adaptivecards.objectmodel.BackgroundImageMode;
 import io.adaptivecards.objectmodel.BaseActionElement;
 import io.adaptivecards.objectmodel.BaseActionElementVector;
 import io.adaptivecards.objectmodel.BaseCardElementVector;
@@ -50,13 +56,15 @@ public class AdaptiveCardRenderer
     {
         private Context m_context;
         private LinearLayout m_layout;
+        private BackgroundImage m_backgroundImageProperties;
 
-        public BackgroundImageLoaderAsync(RenderedAdaptiveCard renderedCard, Context context, LinearLayout layout, String imageBaseUrl)
+        public BackgroundImageLoaderAsync(RenderedAdaptiveCard renderedCard, Context context, LinearLayout layout, String imageBaseUrl, BackgroundImage backgroundImageProperties)
         {
             super(renderedCard, imageBaseUrl);
 
             m_context = context;
             m_layout = layout;
+            m_backgroundImageProperties = backgroundImageProperties;
         }
 
         @Override
@@ -72,7 +80,58 @@ public class AdaptiveCardRenderer
         void onSuccessfulPostExecute(Bitmap bitmap)
         {
             BitmapDrawable background = new BitmapDrawable(m_context.getResources(), bitmap);
-            m_layout.setBackground(background);
+
+            int height = bitmap.getHeight();
+            int width = bitmap.getWidth();
+            switch(m_backgroundImageProperties.GetMode())
+            {
+                case Repeat:
+                    background.setTileModeXY(Shader.TileMode.REPEAT, Shader.TileMode.REPEAT);
+                    m_layout.setBackground(background);
+                    break;
+                case RepeatHorizontally:
+                    background.setTileModeX(Shader.TileMode.REPEAT);
+                    switch (m_backgroundImageProperties.GetVerticalAlignment())
+                    {
+                        case Bottom:
+                            // NOPE: background.setBounds(m_layout.getLeft(), m_layout.getBottom() - height, m_layout.getRight(), m_layout.getBottom());
+                            // NONONONONO: m_layout.setPadding(m_layout.getLeft(),m_layout.getBottom() - height, m_layout.getRight(), m_layout.getBottom());
+                            // NOPE, IGNORED WHEN TILEMODE ENABLED: background.setGravity(Gravity.BOTTOM);
+                            ImageView imageView = new ImageView(m_context);
+                            imageView.setImageDrawable(background);
+                            //imageView.setMinimumWidth(m_layout.getRight());
+                            //imageView.setCropToPadding(false);
+                            imageView.setScaleType(ImageView.ScaleType.FIT_XY);
+
+
+                            m_layout.addView(imageView);
+                            //LinearLayout parentView = (LinearLayout) m_layout.getParent();
+                            //parentView.addView(imageView);
+                            break;
+                        case Center:
+                            //background.setBounds(m_layout.getLeft(), (m_layout.getBottom() - height)/2, m_layout.getRight(), (m_layout.getBottom() + height)/2);
+                            ImageView view = new ImageView(m_context);
+                            view.setImageDrawable(background);
+                            LinearLayout parent = (LinearLayout) m_layout.getParent();
+                            view.setX(parent.getLeft());
+                            view.setY((m_layout.getBottom() - height)/2);
+                            parent.addView(view);
+                            break;
+                        case Top:
+                        default:
+                            background.setBounds(m_layout.getLeft(), m_layout.getTop(), m_layout.getRight(), m_layout.getTop() + height);
+                            break;
+                    }
+                    break;
+                case RepeatVertically:
+                    background.setTileModeY(Shader.TileMode.REPEAT);
+                    background.setBounds(m_layout.getLeft(), m_layout.getTop(), width, m_layout.getLeft() + m_layout.getBottom());
+                    break;
+                case Stretch:
+                default:
+                    m_layout.setBackground(background);
+                    break;
+            }
             m_layout.bringChildToFront(m_layout.getChildAt(0));
         }
     }
@@ -206,7 +265,7 @@ public class AdaptiveCardRenderer
         String imageUrl = adaptiveCard.GetBackgroundImage().GetUrl();
         if (!imageUrl.isEmpty())
         {
-            BackgroundImageLoaderAsync loaderAsync = new BackgroundImageLoaderAsync(renderedCard, context, layout, hostConfig.GetImageBaseUrl());
+            BackgroundImageLoaderAsync loaderAsync = new BackgroundImageLoaderAsync(renderedCard, context, layout, hostConfig.GetImageBaseUrl(), adaptiveCard.GetBackgroundImage());
 
             IOnlineImageLoader onlineImageLoader = CardRendererRegistration.getInstance().getOnlineImageLoader();
             if(onlineImageLoader != null)
